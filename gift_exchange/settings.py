cff05a31ca84eec7e64682bae274ca146e817137
alirizagocerr <alirizagocer4434@gmail.com>
2025-12-10 12:27:01 +0300

    Max length değerleri atandı.

--- gift_exchange/settings.py
+++ gift_exchange/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-change-this-in-production-123456789'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
