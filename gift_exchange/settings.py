e99ab2856a01d12ae7c10d03e9c82e1ed23120d5
Omer Faruk Coskun <omerfarrukcoskun@gmail.com>
2025-12-11 22:44:32 +0300

    Checkin render google stmp settings!

--- gift_exchange/settings.py
+++ gift_exchange/settings.py
@@ -107,10 +107,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py en altı
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp-relay.brevo.com'
-EMAIL_PORT = 587  # 2525 yerine 587 (TLS standardı)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-EMAIL_HOST_USER = os.getenv('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASSWORD')
-EMAIL_TIMEOUT = 30
+EMAIL_HOST_USER = 'iucdevops1453@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # Render'dan alacak
