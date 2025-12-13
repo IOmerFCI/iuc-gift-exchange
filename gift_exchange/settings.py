66f297436fa87f9b33eb02256d5a3fd34bc751ea
Altan Tarı <altantari@gmail.com>
2025-12-13 05:20:53 +0300

    Email adresi guncellendi

--- gift_exchange/settings.py
+++ gift_exchange/settings.py
@@ -132,6 +132,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'omerfaruk14411441@gmail.com'
+EMAIL_HOST_USER = 'devopsiuc@gmail.com'
 # Şifreyi asla kodun içine yazma, Env'den çek
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
